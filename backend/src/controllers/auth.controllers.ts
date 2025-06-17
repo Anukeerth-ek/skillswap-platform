36c6bd7613979fd766b47f7b0fbf065f0c3be728
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-06-17 12:32:52 +0530

    feat: added signup auth controller

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -1,2 +1,25 @@
+import {Request, Response} from 'express'
+import prisma from '../prismaClient'
+import { hashPassword } from '../utils/hash.utils'
 
-import {} from 'express'
+export const signup = async(req:Request, res: Response)=> {
+    const {name, email, password} = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({message: "Email and passsword is required"});
+    }
+
+    const existingUser = await prisma.user.findUnique({where: email})
+
+    if(existingUser) {
+        return res.status(409).json({message: 'User already exist'})
+    }
+
+    const hashdedUserPassword = await hashPassword(password)
+
+    const newUser = await prisma.user.create({
+        data: {name, email, password: hashdedUserPassword},
+    })
+
+    res.status(201).json({message: "user created", userId: newUser.id})
+}
