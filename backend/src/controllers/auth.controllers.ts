c70961ff35bf0571925ea558d57468c2e09bed21
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-06-18 13:55:40 +0530

    fix: login, signup issue, added jwt support

--- backend/src/controllers/auth.controllers.ts
+++ backend/src/controllers/auth.controllers.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import prisma from "../prismaClient";
 import { comparePassword, hashPassword } from "../utils/hash.utils";
 
+const jwt = require("jsonwebtoken");
+
 export const signup = async (req: Request, res: Response) => {
      const { name, email, password } = req.body;
 
@@ -21,7 +23,7 @@ export const signup = async (req: Request, res: Response) => {
           data: { name, email, password: hashdedUserPassword },
      });
 
-     res.status(201).json({ message: "user created", userId: newUser.id });
+     res.status(201).json({ message: "user created", user: { id: newUser.id, email: newUser.password } });
 };
 
 export const login = async (req: Request, res: Response) => {
@@ -35,7 +37,13 @@ export const login = async (req: Request, res: Response) => {
 
      const isValid = await comparePassword(passsword, user?.password);
 
-     if(!isValid) {
-        return res.status(404).json({message: "Invalid Credientials"})
+     if (!isValid) {
+          return res.status(404).json({ message: "Invalid Credientials" });
      }
+
+     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1day" });
+
+     res.json({ token });
 };
+
+export default { signup, login };
