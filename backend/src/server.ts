cab0d568453754d7c8d0a53e25f9b28390c0ef57
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-05 00:28:43 +0530

    feat: added cors support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,30 @@
 import express = require("express");
 import dotenv = require("dotenv");
+import cors = require("cors"); 
+
 import skillRoutes from "./routes/skillRoutes";
 import userRoutes from "./routes/userRoutes";
-import bookSessionRoutes from "./routes/bookSessionRoutes"
-import authRoutes from "./routes/authRoutes"
-import learnerRoutes from "./routes/learnerRoutes"
-import mentorRoutes from "./routes/mentorRoutes"
+import bookSessionRoutes from "./routes/bookSessionRoutes";
+import authRoutes from "./routes/authRoutes";
+import learnerRoutes from "./routes/learnerRoutes";
+import mentorRoutes from "./routes/mentorRoutes";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors()); // ✅ Enable CORS for all origins
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/skills", skillRoutes);
 app.use("/api/bookSession", bookSessionRoutes);
 app.use("/api/profile", userRoutes);
-app.use("/api/learnersBooking", learnerRoutes)
-app.use("/api/mentorBooking", mentorRoutes)
+app.use("/api/learnersBooking", learnerRoutes);
+app.use("/api/mentorBooking", mentorRoutes);
 
 const PORT = process.env.PORT || 4500;
 
 app.listen(PORT, () => {
-     console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
