97c9f0655796c8b4005f7de01fd3aac38e3f51dc
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-05 01:56:45 +0530

    fix: removed build files

--- backend/dist/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const { signup, login } = require('../controllers/auth.controllers');
-const router = express.Router();
-router.post('/signup', signup);
-router.post('/login', login);
-exports.default = router;
