86b2aa7de55fe2e76908307bfa2b5c41906af059
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-09-18 21:54:23 +0530

    Feat: fixed profile page bug

--- frontend/src/app/components/HomeSections/searchResult/index.tsx
+++ frontend/src/app/components/HomeSections/searchResult/index.tsx
@@ -31,7 +31,7 @@ export const SearchResults = ({ users, loading,  }: SearchResultsProps) => {
      }, [users]);
 
      if (loading) return <p>Loading...</p>;
-
+     console.log("user", users)
      return (
           <>
                <div className="flex-1 p-6">
