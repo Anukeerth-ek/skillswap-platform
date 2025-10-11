cbdc38fbd2d79d05ea806a4dd478800a5a81b94b
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-10-11 13:38:58 +0530

    feat: properly re render user list

--- frontend/src/app/hooks/useGetMyProfile.ts
+++ frontend/src/app/hooks/useGetMyProfile.ts
@@ -4,48 +4,51 @@
 import { useEffect, useState } from "react";
 
 export type User = {
-  id: string; // ✅ Add this field
-  name: string;
-  avatarUrl?: string;
-  email?: string;
-  role?: "LEARNER" | "MENTOR" | string;
-  createdAt?: string;
+     id: string; // ✅ Add this field
+     name: string;
+     avatarUrl?: string;
+     email?: string;
+     role?: "LEARNER" | "MENTOR" | string;
+     createdAt?: string;
 };
 
-
 export const useGetMyProfile = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
+     const [user, setUser] = useState<User | null>(null);
+     const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        setLoading(false);
-        return;
-      }
+     useEffect(() => {
+          const fetchUser = async () => {
+               const token: any = localStorage.getItem("token");
+               localStorage.setItem("token", token);
+               window.dispatchEvent(new Event("userLoggedIn"));
+               if (!token) {
+                    setLoading(false);
+                    return;
+               }
 
-      try {
-        const res = await fetch("http://localhost:4000/api/profile/me", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+               try {
+                    const res = await fetch("http://localhost:4000/api/profile/me", {
+                         headers: {
+                              Authorization: `Bearer ${token}`,
+                         },
+                    });
 
-        if (!res.ok) console.log("not authorized")
+                    if (!res.ok) console.log("not authorized");
 
-        const data = await res.json();
-        setUser(data.user);
-      } catch (err) {
-        console.error("Failed to fetch user:", err);
-        setUser(null);
-      } finally {
-        setLoading(false);
-      }
-    };
+                    const data = await res.json();
+                    setUser(data.user);
+               } catch (err) {
+                    console.error("Failed to fetch user:", err);
+                    setUser(null);
+               } finally {
+                    setLoading(false);
+               }
+          };
 
-    fetchUser();
-  }, []);
+          fetchUser();
+          window.addEventListener("userLoggedIn", fetchUser);
+          return () => window.removeEventListener("userLoggedIn", fetchUser);
+     }, []);
 
-  return { user, loading };
+     return { user, loading };
 };
